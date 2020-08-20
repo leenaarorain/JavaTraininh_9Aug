efa9ab5272c267c7e73acab58208b015e0e94e96
Abhishek <34632758+Abhi03021990@users.noreply.github.com>
2020-08-20 22:49:12 +0530

    code 20 aug
    20th August Class . Loops

---
+++ ForLoopEg.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class ForLoopEg {
+
+	public static void main(String[] args) {
+		int i=0;
+		// TODO Auto-generated method stub
+		for (int counter = 0;counter<3 ; i++) {
+			System.out.println("Hi");
+		}
+
+	}
+
+}
---
+++ LoopsEg.java
@@ -0,0 +1,27 @@
+package loops;
+
+import java.util.Scanner;
+
+public class LoopsEg {
+
+	public static void main(String[] args) {
+		// write a code to add n natural mumbers 
+		int sum=0;
+		System.out.println("-----------------------------------------------------");
+		System.out.println("------------Program to find sum of n natural number--");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Enter the value of n");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for (int i = 1; i <= n; i++) {
+			sum = sum+i;
+			System.out.println("Value of i ="+i);
+			System.out.println("Value of sum ="+sum);
+			
+		}
+		System.out.println("addition of "+n+" natural numbers are "+sum);
+		
+
+	}
+
+}
